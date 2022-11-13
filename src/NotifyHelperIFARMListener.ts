0154abb7c31f7575b224114c461894b85ff77d80
alex <alexandersazonov0@gmail.com>
2022-11-13 13:29:23 +0300

    Add matic network

--- src/NotifyHelperIFARMListener.ts
+++
@@ -1,9 +0,0 @@
-import { loadOrCreatePotPool } from "./utils/PotPool";
-import { NotifyPoolsCall } from "../generated/NotifyHelperIFARMListener/NotifyHelperIFARMContract";
-
-export function handlePotNotifyPools(call: NotifyPoolsCall): void {
-  const pools = call.inputs.pools
-  for (let i = 0; i < pools.length; i++) {
-    loadOrCreatePotPool(pools[i], call.block)
-  }
-}
