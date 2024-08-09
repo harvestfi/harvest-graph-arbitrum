d9422318d1d14765b9a032d7479310c14a13ee68
alex <alexandersazonov0@gmail.com>
2024-08-09 22:29:10 +0300

    Draft life time profit version

--- src/types/UserBalance.ts
+++ src/types/UserBalance.ts
@@ -1,5 +1,12 @@
 import { Address, BigDecimal, BigInt, ethereum } from "@graphprotocol/graph-ts";
-import { UserBalance, UserBalanceHistory, UserTransaction, Vault } from "../../generated/schema";
+import {
+  UserBalance,
+  UserBalanceHistory,
+  UserProfit,
+  UserProfitHistory, UserTotalProfit,
+  UserTransaction,
+  Vault,
+} from '../../generated/schema';
 import { VaultContract } from "../../generated/templates/VaultListener/VaultContract";
 import { ERC20 } from "../../generated/Controller/ERC20";
 import { pow } from "../utils/MathUtils";
@@ -27,6 +34,21 @@ export function createUserBalance(vaultAddress: Address, amount: BigInt, benefic
       userBalance.vault = vault.id
       userBalance.value = BigDecimal.zero()
       userBalance.userAddress = beneficary.toHex()
+      userBalance.underlyingBalance = BigDecimal.zero()
+    }
+
+    const delimiter = pow(BD_TEN, vault.decimal.toI32());
+    const sharePriceFormatted = vault.lastSharePrice.divDecimal(delimiter);
+    if (isDeposit) {
+      userBalance.underlyingBalance = userBalance.underlyingBalance.plus(amount.divDecimal(delimiter).times(sharePriceFormatted))
+    } else {
+      userBalance.underlyingBalance = userBalance.underlyingBalance.minus(amount.divDecimal(delimiter).times(sharePriceFormatted))
+    }
+
+    let profit = BigDecimal.zero();
+    if (userBalance.underlyingBalance.lt(BigDecimal.zero())) {
+      profit = userBalance.underlyingBalance.neg().times(vault.priceUnderlying)
+      userBalance.underlyingBalance = BigDecimal.zero()
     }
 
     userBalance.poolBalance = poolBalance
@@ -34,11 +56,14 @@ export function createUserBalance(vaultAddress: Address, amount: BigInt, benefic
     userBalance.value = value
 
     userBalance.save()
-    const userBalanceHistory = new UserBalanceHistory(`${tx.hash.toHex()}-${beneficary.toHex()}-${vault.id}-${isDeposit.toString()}`)
+
+    const historyId = `${tx.hash.toHex()}-${beneficary.toHex()}-${vault.id}-${isDeposit.toString()}`;
+    const userBalanceHistory = new UserBalanceHistory(historyId)
     userBalanceHistory.createAtBlock = block.number
     userBalanceHistory.timestamp = block.timestamp
     userBalanceHistory.userAddress = beneficary.toHex()
     userBalanceHistory.vault = vault.id
+    userBalanceHistory.underlyingBalance = userBalance.underlyingBalance
     userBalanceHistory.transactionType = isDeposit
       ? 'Deposit'
       : 'Withdraw'
@@ -63,6 +88,41 @@ export function createUserBalance(vaultAddress: Address, amount: BigInt, benefic
     userTransaction.sharePrice = vaultContract.getPricePerFullShare()
     userTransaction.value = amount
     userTransaction.save()
+
+    if (profit.gt(BigDecimal.zero())) {
+
+      // calculate user profit
+      let userProfit = UserProfit.load(userBalanceId);
+      if (userProfit == null) {
+        userProfit = new UserProfit(userBalanceId);
+        userProfit.userAddress = beneficary.toHex();
+        userProfit.vault = vault.id;
+        userProfit.value = BigDecimal.zero();
+      }
+      userProfit.value = userProfit.value.plus(profit)
+      userProfit.save();
+
+      // calculate user profit history
+      const userProfitHistory = new UserProfitHistory(historyId);
+      userProfitHistory.userAddress = beneficary.toHex();
+      userProfitHistory.transactionType = userBalanceHistory.transactionType
+      userProfitHistory.vault = vault.id;
+      userProfitHistory.value = userProfit.value;
+      userProfitHistory.sharePrice = vault.lastSharePrice;
+      userProfitHistory.transactionAmount = amount;
+      userProfitHistory.createAtBlock = block.number
+      userProfitHistory.timestamp = block.timestamp
+      userProfitHistory.save();
+
+      // total profit
+      let userTotalProfit = UserTotalProfit.load(beneficary.toHex());
+      if (userTotalProfit == null) {
+        userTotalProfit = new UserTotalProfit(beneficary.toHex());
+        userTotalProfit.value = BigDecimal.zero();
+      }
+      userTotalProfit.value = userTotalProfit.value.plus(profit)
+      userTotalProfit.save();
+    }
   }
 }
 
