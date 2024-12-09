d80d80522891c6d0fafcf5cf81a65d1091309c16
alex <alexandersazonov0@gmail.com>
2024-12-09 09:52:38 +0300

    Add rewardPaidEntity for pools

--- src/PotPoolListener.ts
+++ src/PotPoolListener.ts
@@ -1,8 +1,15 @@
-import { PotPoolContract } from "../generated/templates/PotPoolListener/PotPoolContract";
+import { PotPoolContract, RewardPaid } from '../generated/templates/PotPoolListener/PotPoolContract';
 import { saveReward } from "./types/Reward";
-import { log } from "@graphprotocol/graph-ts";
+import { BigDecimal, BigInt, Bytes, log } from '@graphprotocol/graph-ts';
 import { saveApyReward } from "./types/Apy";
 import { RewardAdded } from "../generated/templates/VaultListener/PotPoolContract";
+import { BD_TEN, BI_EVERY_24_HOURS } from './utils/Constant';
+import { pow } from './utils/MathUtils';
+import { getPriceForCoin } from './utils/PriceUtils';
+import { loadOrCreateERC20Token } from './types/Token';
+import { loadOrCreatePotPool } from './types/PotPool';
+import { store } from '@graphprotocol/graph-ts';
+import { RewardPaidEntity, TokenPrice } from '../generated/schema';
 
 export function handleRewardAdded(event: RewardAdded): void {
   const poolAddress = event.address
@@ -27,4 +34,38 @@ export function handleRewardAdded(event: RewardAdded): void {
 
   saveReward(poolAddress, tryRewardToken.value, tryRewardRate.value, tryPeriodFinish.value, rewardAmount, event.transaction, event.block)
   saveApyReward(poolAddress, tryRewardToken.value, tryRewardRate.value, tryPeriodFinish.value, event.transaction, event.block)
+}
+
+export function handleRewardPaid(event: RewardPaid): void {
+  const rewardPaid = new RewardPaidEntity(Bytes.fromUTF8(`${event.transaction.hash.toHex()}-${event.logIndex.toString()}`));
+  rewardPaid.userAddress = event.params.user.toHexString();
+  rewardPaid.pool = loadOrCreatePotPool(event.address, event.block).id;
+  rewardPaid.value = event.params.reward;
+  // price
+  const rewardToken = loadOrCreateERC20Token(event.params.rewardToken);
+  // TODO move logic token price for all project
+  const tokenPriceId = event.params.rewardToken.toHexString();
+  let tokenPrice = TokenPrice.load(tokenPriceId);
+  if (tokenPrice == null) {
+    tokenPrice = new TokenPrice(tokenPriceId);
+    const price = getPriceForCoin(event.params.rewardToken);
+    let priceBD = BigDecimal.zero();
+
+    if (price.gt(BigInt.zero())) {
+      priceBD = price.divDecimal(pow(BD_TEN, rewardToken.decimals));
+    }
+    tokenPrice.price = priceBD;
+    tokenPrice.timestamp = event.block.timestamp;
+    tokenPrice.save();
+  }
+
+  rewardPaid.price = tokenPrice.price;
+  rewardPaid.token = rewardToken.id;
+  rewardPaid.timestamp = event.block.timestamp;
+  rewardPaid.createAtBlock = event.block.number;
+  rewardPaid.save();
+
+  if (tokenPrice.timestamp.plus(BI_EVERY_24_HOURS).lt(event.block.timestamp)) {
+    store.remove('TokenPrice', tokenPriceId);
+  }
 }
--- src/types/PotPool.ts
+++ src/types/PotPool.ts
@@ -8,33 +8,29 @@ import { PotPoolContract } from "../../generated/templates/VaultListener/PotPool
 const TYPE = 'PotPool'
 
 
-export function loadOrCreatePotPool(poolAddress: Address, ethBlock: ethereum.Block): void {
+export function loadOrCreatePotPool(poolAddress: Address, ethBlock: ethereum.Block): Pool {
   let pool = Pool.load(poolAddress.toHex())
   if (pool == null) {
     let poolContract = PotPoolContract.bind(poolAddress)
     let vaultAddress = poolContract.lpToken();
     let tryRewardToken = poolContract.try_rewardToken();
-    if (tryRewardToken.reverted) {
-      return;
+    let rewardTokens: string[] = [];
+    if (!tryRewardToken.reverted) {
+      let rewardToken = loadOrCreateERC20Token(tryRewardToken.value)
+      rewardTokens = [rewardToken.id];
     }
-    let rewardToken = loadOrCreateERC20Token(tryRewardToken.value)
-    let pool = new Pool(poolAddress.toHex())
+    pool = new Pool(poolAddress.toHex())
     pool.timestamp = ethBlock.timestamp
     pool.createAtBlock = ethBlock.number
     pool.vault = vaultAddress.toHex()
     pool.type = TYPE
-    pool.rewardTokens = [rewardToken.id]
+    pool.rewardTokens = rewardTokens
     pool.save()
 
-    let vault = Vault.load(vaultAddress.toHex())
-    if (vault != null) {
-      vault.pool = poolAddress.toHex()
-      vault.save()
-    } else {
-      vault = loadOrCreateVault(vaultAddress, ethBlock)
-      vault.pool = poolAddress.toHex()
-      vault.save()
-    }
+    const vault = loadOrCreateVault(vaultAddress, ethBlock)
+    vault.pool = poolAddress.toHex()
+    vault.save()
     PotPoolListener.create(poolAddress);
   }
+  return pool;
 }
